66d81080ea8142f2b55594b0cd8f09b3a337d02d
Yhomero33 <adelaido.cano.96@gmail.com>
2017-08-24 09:27:46 -0600

    Initial Commit adding structure of directories and creating the first file

---
+++ www/jS/app.js
@@ -0,0 +1,18 @@
+
+var base =5;
+var altura = 7;
+var area = altura * base;
+console.log("El área de mi rectangulo es igual: "+ area);
+
+var radio = 5;
+var areaCirculo = Math.PI * Math.pow(r,2);
+console.log("El área de mi circulo es igual: "+areaCirculo);
+
+var bt = 6;
+var at = 8;
+var areaTriangulo = (bt * at)/2;
+console.log("El área de mi triangulo es igual: "+areaTriangulo);
+
+var rE=5;
+var volumen = (4 * Math.PI * Math.pow(rE,3))/3;
+console.log("El volumen de mi esfera es igual: "+volumen);
